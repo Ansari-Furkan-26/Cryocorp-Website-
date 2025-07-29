2716818845a7d31e5d12646e7bcaf952e7686ead
saiganesh-sristla <saiganesh3108@gmail.com>
2025-07-29 15:49:12 +0530

    init

--- Frontend/src/components/Quiz/QuizInfoForm.tsx
+++ Frontend/src/components/Quiz/QuizInfoForm.tsx
@@ -56,11 +56,11 @@ const QuizModal: React.FC<QuizModalProps> = ({ isOpen, onClose }) => {
             // const name = (e.target as any).name.value;
             const email = (e.target as any).email.value;
             // store the data in backend
-            await axios.post(" https://api.cryocorp.in/api/email/emails", {
+            window.location.href = `/quiz/${link}.html`;
+            await axios.post("https://api.cryocorp.in/api/email/emails", {
               email,
             });
             console.log("Submitted:", { email });
-            window.location.href = `/quiz/${link}.html`;
             onClose();
           }}
           className="space-y-5"
